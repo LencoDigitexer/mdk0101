9be87e98e984812ec1f694e661cde46390cf7640
Илья Евсеенко <44779327+LencoDigitexer@users.noreply.github.com>
2021-12-12 20:02:51 +0300

    практика 15-17

---
+++ pr15-17_EIN/pr15-17_EIN/about.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace pr15_17_EIN
+{
+    partial class About
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::pr15_17_EIN.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 104);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Notepad C# 2021 All rights reserved.";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(13, 124);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(128, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "www.notepadcsharp.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 185);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "About";
+            this.Text = "About Notepad C#";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ pr15-17_EIN/pr15-17_EIN/blank.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pr15_17_EIN
+{
+    public partial class blank : Form
+    {
+        public string DocName = "";
+        private string BufferText = "";
+
+        public bool IsSaved = false;
+
+        public blank()
+        {
+            InitializeComponent();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+        //Создаем метод Open, в качестве параметра объявляем строку адреса  файла.
+        public void Open(string OpenFileName)
+        {
+            //Если файл не выбран, возвращаемся назад (появится встроенное предупреждение)
+            if (OpenFileName == "")
+            {
+                return;
+            }
+            else
+            {
+                //Создаем новый объект StreamReader и передаем ему переменную //OpenFileName
+                StreamReader sr = new StreamReader(OpenFileName);
+                //Читаем весь файл и записываем его в richTextBox1
+                richTextBox1.Text = sr.ReadToEnd();
+                // Закрываем поток
+                sr.Close();
+                //Переменной DocName присваиваем адресную строку
+                DocName = OpenFileName;
+            }
+        }//Создаем метод Save, в качестве параметра объявляем строку адреса  файла.
+        public void Save(string SaveFileName)
+        {
+            //Если файл не выбран, возвращаемся назад (появится встроенное предупреждение)
+            if (SaveFileName == "")
+            {
+                return;
+            }
+            else
+            {
+                //Создаем новый объект StreamWriter и передаем ему переменную //OpenFileName
+                StreamWriter sw = new StreamWriter(SaveFileName);
+                //Содержимое richTextBox1 записываем в файл
+                sw.WriteLine(richTextBox1.Text);
+                //Закрываем поток
+                sw.Close();
+                //Устанавливаем в качестве имени документа название сохраненного файла
+                DocName = SaveFileName;
+            }
+        }
+
+        // Вырезание текста
+        public void Cut()
+        {
+            this.BufferText = richTextBox1.SelectedText;
+            richTextBox1.SelectedText = "";
+        }
+
+        // Копирование текста
+        public void Copy()
+        {
+            this.BufferText = richTextBox1.SelectedText;
+        }
+
+        // Вставка
+        public void Paste()
+        {
+            richTextBox1.SelectedText = this.BufferText;
+        }
+
+        // Выделение всего текста — используем свойство SelectAll элемента управления RichTextBox 
+        public void SelectAll()
+        {
+            richTextBox1.SelectAll();
+        }
+
+        // Удаление
+        public void Delete()
+        {
+            richTextBox1.SelectedText = "";
+            this.BufferText = "";
+        }
+
+        private void blank_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //Если переменная IsSaved имеет значение true, т. е.  новый документ 
+            //был сохранен (Save As) или в открытом документе были сохранены изменения (Save), то 
+            //выполняется условие
+            if (IsSaved == true)
+                //Появляется диалоговое окно, предлагающее сохранить документ.
+                if (MessageBox.Show("Do you want save changes in " + this.DocName + "?", "Message", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question) == DialogResult.Yes)
+                //Если была нажата  кнопка Yes, вызываем метод Save
+                {
+                    this.Save(this.DocName);
+                }
+        }
+
+
+
+    }
+}
