09a7e6c1cde1665f2accaa5ed07a002571c428c2
Илья Евсеенко <addminuser258@gmail.com>
2021-12-22 17:16:12 +0300

    pr07v15

---
+++ v15/pr07_v15_KVA/pr07_v15_KVA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pr07_v15_KVA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[3];
+            int sum = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                
+                Console.WriteLine("Цена {0}-ого товара ", i+1);
+                n[i] = Convert.ToInt32(Console.ReadLine());
+                sum = sum + n[i];
+            }
+            if (sum > 2000)
+            {
+                sum = sum - ((sum / 100) * 3);
+            }
+            Console.WriteLine("Итоговая стоимость = " + sum);
+
+            
+        }
+    }
+}
---
+++ v15/pr07_v15_KVA/pr07_v15_KVA/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("pr07_v15_KVA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pr07_v15_KVA")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ab7716f7-0862-40ce-a447-419403d59af4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
